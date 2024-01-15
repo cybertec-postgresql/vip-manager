95bca1e495ac3a1e4bbde9935d6830ad76aca3ce
dreamingdeer <dreamingdeer@yandex.ru>
2024-01-15 22:48:51 +0700

    feat(etcd-cheker) watch on trigger key

--- checker/etcd_leader_checker.go
+++ checker/etcd_leader_checker.go
@@ -10,14 +10,14 @@ import (
 	"time"
 
 	"github.com/cybertec-postgresql/vip-manager/vipconfig"
-	client "go.etcd.io/etcd/client/v3"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 // EtcdLeaderChecker is used to check state of the leader key in Etcd
 type EtcdLeaderChecker struct {
 	key      string
 	nodename string
-	kapi     client.KV
+	client   *clientv3.Client
 }
 
 // naming this c_conf to avoid conflict with conf in etcd_leader_checker.go
@@ -71,7 +71,7 @@ func NewEtcdLeaderChecker(con *vipconfig.Config) (*EtcdLeaderChecker, error) {
 		return nil, err
 	}
 
-	cfg := client.Config{
+	cfg := clientv3.Config{
 		Endpoints: eConf.Endpoints,
 		TLS:       tlsConfig,
 		// see bug https://github.com/etcd-io/etcd/issues/8905 (15 min for default)
@@ -81,50 +81,53 @@ func NewEtcdLeaderChecker(con *vipconfig.Config) (*EtcdLeaderChecker, error) {
 		Username:             eConf.EtcdUser,
 		Password:             eConf.EtcdPassword,
 	}
-	c, err := client.New(cfg)
+	c, err := clientv3.New(cfg)
 	if err != nil {
 		return nil, err
 	}
-	e.kapi = c.KV
+	e.client = c
 	return e, nil
 }
 
 // GetChangeNotificationStream checks the status in the loop
 func (e *EtcdLeaderChecker) GetChangeNotificationStream(ctx context.Context, out chan<- bool) error {
 	var state bool
-	var alreadyConnected = false
-checkLoop:
-	for {
-		resp, err := e.kapi.Get(ctx, e.key)
 
-		if err != nil {
-			if ctx.Err() != nil {
-				break checkLoop
-			}
-			log.Printf("etcd error: %s", err)
-			out <- false
-			time.Sleep(time.Duration(eConf.Interval) * time.Millisecond)
-			continue
+	defer e.client.Close()
+
+	// get current state from etcd
+	resp, err := e.client.Get(ctx, e.key)
+	if err != nil {
+		if ctx.Err() != nil {
+			return ctx.Err()
 		}
+		log.Printf("etcd error: %s", err)
+		out <- false
+		time.Sleep(time.Duration(eConf.Interval) * time.Millisecond)
+	}
 
-		if !alreadyConnected {
-			log.Printf("etcd checker started up, found key %s", e.key)
-			alreadyConnected = true
-		}
+	// process responce
+	for _, kv := range resp.Kvs {
+		log.Println("Current Leader from DCS:", string(kv.Value))
+		state = string(kv.Value) == e.nodename
+	}
 
-		for _, kv := range resp.Kvs {
-			if eConf.Verbose {
-				log.Println("Leader from DCS:", string(kv.Value))
-			}
-			state = string(kv.Value) == e.nodename
-		}
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case out <- state:
+		time.Sleep(time.Duration(eConf.Interval) * time.Millisecond)
+	}
 
-		select {
-		case <-ctx.Done():
-			break checkLoop
-		case out <- state:
-			time.Sleep(time.Duration(eConf.Interval) * time.Millisecond)
-			continue
+	watchChan := e.client.Watch(ctx, e.key)
+	fmt.Println("set WATCH on " + e.key)
+
+	// process watching changes for key
+	for watchResp := range watchChan {
+		for _, event := range watchResp.Events {
+			state = string(event.Kv.Value) == e.nodename
+			out <- state
+			fmt.Printf("Event received! %s executed on %q with value %q\n", event.Type, event.Kv.Key, event.Kv.Value)
 		}
 	}
 
