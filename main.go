4869dca373e4c124b55580e13977733ec7581a67
zhongyibill <zhongyi_bill@163.com>
2024-05-10 18:59:23 +0800

    [!] add support for IPv6 (#225)
    Co-authored-by: Pavlo Golub <pavlo.golub@gmail.com>

--- main.go
+++ main.go
@@ -24,11 +24,14 @@ var (
 )
 
 func getMask(vip netip.Addr, mask int) net.IPMask {
-	if mask > 0 || mask < 33 {
-		return net.CIDRMask(mask, 32)
+	if vip.Is4() { //IPv4
+		if mask > 0 && mask < 33 {
+			return net.CIDRMask(mask, 32)
+		}
+		var ip net.IP = vip.AsSlice()
+		return ip.DefaultMask()
 	}
-	var ip net.IP = vip.AsSlice()
-	return ip.DefaultMask()
+	return net.CIDRMask(mask, 128) //IPv6
 }
 
 func getNetIface(iface string) *net.Interface {
