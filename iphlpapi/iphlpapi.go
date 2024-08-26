d25a8b08db9795da330a8634e1999d7833ea29ea
Pavlo Golub <pavlo.golub@gmail.com>
2024-08-26 08:55:27 +0000

    [-] fix "`run.skip-dirs` is deprecated"

--- iphlpapi/iphlpapi.go
+++ iphlpapi/iphlpapi.go
@@ -1,3 +1,6 @@
+//go:build windows
+// +build windows
+
 package iphlpapi
 
 //sys	AddIPAddress(Address uint32, IpMask uint32, IfIndex uint32, NTEContext *uint32, NTEInstance *uint32) (errcode error) = iphlpapi.AddIPAddress
--- iphlpapi/mksyscall.go
+++ iphlpapi/mksyscall.go
@@ -1,5 +1,8 @@
+//go:build windows
+// +build windows
+
 // +build generate
 
 package iphlpapi
 
-//go:generate go run golang.org/x/sys/windows/mkwinsyscall -output ziphlapi.go iphlpapi.go
+//go:generate go run golang.org/x/sys/windows/mkwinsyscall -output ziphlapi_windows.go iphlpapi.go
